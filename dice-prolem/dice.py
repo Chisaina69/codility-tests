f615bec535f8780f4fdce14cc2abe8e4516931ff
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-10-18 09:49:09 +0300

    Dice problem solved

--- dice-prolem/dice.py
+++ dice-prolem/dice.py
@@ -1,2 +1,19 @@
-def solution(A,f,M):
-    ()
+def solution(A, F, M):
+    N = len(A)
+    total_sum = M * (N + F)  
+    existing_sum = sum(A)  
+
+    missing_sum = total_sum - existing_sum  
+    if missing_sum > F * 6 or missing_sum < F: 
+        return [0]
+
+    result = [6] * F
+    missing_sum -= 6 * F
+
+    for i in range(F):
+        if missing_sum > 0:
+            decrease = min(5, missing_sum)  
+            result[i] -= decrease
+            missing_sum -= decrease
+
+    return result
